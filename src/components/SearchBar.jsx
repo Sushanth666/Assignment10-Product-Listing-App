221680da5ae5ff822299e6c24fc1269e5f3d9654
B V Sree Sushanth <bvsreesushanth29802@gmail.com>
2025-07-31 17:07:40 +0530

    Update SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-const Sort = ({ sortOption, onSortChange }) => (
-  <select
+const SearchBar = ({ searchTerm, onSearchChange }) => (
+  <input
+    type="text"
     className="form-control"
-    value={sortOption}
-    onChange={e => onSortChange(e.target.value)}
-  >
-    <option value="none">Sort: None</option>
-    <option value="price-asc">Price: Low to High</option>
-    <option value="price-desc">Price: High to Low</option>
-    <option value="rating-desc">Rating: High to Low</option>
-  </select>
+    placeholder="Search products..."
+    value={searchTerm}
+    onChange={e => onSearchChange(e.target.value)}
+  />
 );
 
-export default Sort;
+export default SearchBar;
