9edffd0d0aab43d14769694ed614697c5f2977e0
B V Sree Sushanth <bvsreesushanth29802@gmail.com>
2025-07-31 16:56:56 +0530

    Update Filter.jsx

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-function Filter({ categories, selectedCategory, onCategoryChange }) {
-  return (
-    <select
-      className="form-control d-inline-block w-auto mb-3"
-      value={selectedCategory}
-      onChange={e => onCategoryChange(e.target.value)}
-    >
-      <option value="All">All</option>
-      {categories.map(cat => (
-        <option key={cat} value={cat}>{cat}</option>
-      ))}
-    </select>
-  );
-}
+
+const Filter = ({ categories, selectedCategory, onCategoryChange }) => (
+  <select
+    className="form-control"
+    value={selectedCategory}
+    onChange={e => onCategoryChange(e.target.value)}
+  >
+    <option value="All">All</option>
+    {categories.map(cat => (
+      <option key={cat} value={cat}>{cat}</option>
+    ))}
+  </select>
+);
+
 export default Filter;
